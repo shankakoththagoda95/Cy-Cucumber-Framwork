395726bd99265023e2cb21ce4b8cacb76ab17573
shankakoththagoda95 <shankakoththagoda@gmail.com>
2023-03-19 21:17:23 +0100

    parameterize the URL function

--- cypress/support/step_definitions/stepDef.cy.js
+++ cypress/support/step_definitions/stepDef.cy.js
@@ -2,12 +2,36 @@
 import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
 
 
+function ReturenElement(Testelement){
 
-Given("Start to type your Given step here", () => {
-  cy.log("Nothing pass")
+  cy.readFile('cypress/Locators/test.json').then((json) => {
+    cy.log(json.Testelement.path)
+   });
   
-  cy.visit('https://www.google.com')
- 
+  // cy.log("cypress/Locators/*.json")
+  // cy.log(config)
+  
+  // fetch("cypress/Locators/*.json")
+  // .then(function (response) {
+  //   return response.json();
+  // })
+  // .then(function (data) {
+  //   for (let i = 0; i < data.length; i++) {
+  //     console.log(data[i]);
+  //   }
+  // })
+}
+
+Given('I vist the {string} page', (weburl) => {
+  try {
+    cy.visit(weburl)
+  } catch (error) {
+    throw error
+  }
+});
+
+Then('I click on the {string}', (webelement) => {
+  cy.get(ReturenElement(webelement)).click()
 });
 
 // Then("I should see a search bar", () => {
